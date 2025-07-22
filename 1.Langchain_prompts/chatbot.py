18b148fc163b20dc8fc9aa1bdc19f7aa531ddda1
Pankaj Yadav <105292769+Py7949@users.noreply.github.com>
2025-07-23 01:37:06 +0530

    Add files via upload

--- 1.Langchain_prompts/chatbot.py
+++ 1.Langchain_prompts/chatbot.py
@@ -1,22 +1,37 @@
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_community.llms import HuggingFacePipeline
 from transformers import pipeline
-from langchain.llms import HuggingFacePipline
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain_core.language_models import BaseLanguageModel
 
-pipe = pipeline("text2text-generation", model="google/flan-t5-base")
 
-llm = HuggingFacePipline(pipeline=pipe)
+text_generator = pipeline("text-generation", model="gpt2", max_new_tokens=100)
 
-prompt = PromptTemplate(
-    input_variables=['question'],
-    template='You are a helpful assistant.Answer this question : {question}'
-)
-
-chain = LLMChain(llm=llm, prompt=prompt)
+model = HuggingFacePipeline(pipeline=text_generator)
+chat_history = [
+    SystemMessage(content='You are a helpful AI assistant.')
+]
 
 while True:
-    user_input = ('You: ')
-    if user_input.lower() in ['exist', 'quit']:
+    user_input = input("You: ")
+    if user_input.lower() == "exit":
         break
-    response = chain.run(user_input)
-    print("Bot:", response)
+
+    chat_history.append(HumanMessage(content=user_input))
+
+    
+    formatted_prompt = "\n".join([
+        f"User: {m.content}" if isinstance(m, HumanMessage) else
+        f"Assistant: {m.content}" if isinstance(m, AIMessage) else
+        f"{m.content}" for m in chat_history
+    ]) + "\nAssistant:"
+
+    result = model.invoke(formatted_prompt)
+    chat_history.append(AIMessage(content=result))
+    print("AI:", result)
+
+
+print("\n--- Chat History ---")
+for msg in chat_history:
+    role = msg.__class__.__name__.replace("Message", "")
+    print(f"{role}: {msg.content}")
+ 
